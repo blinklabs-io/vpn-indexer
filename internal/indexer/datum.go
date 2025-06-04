58c400a12cc02ba49fc3289b783e78f0cf0908bf
Aurora Gaffney <aurora@blinklabs.io>
2025-06-04 13:42:56 -0400

    feat: generate client from indexer events (#17)
    Fixes #6
    
    Signed-off-by: Aurora Gaffney <aurora@blinklabs.io>

---
+++ internal/indexer/datum.go
@@ -0,0 +1,46 @@
+// Copyright 2025 Blink Labs Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package indexer
+
+import (
+	"errors"
+
+	"github.com/blinklabs-io/gouroboros/cbor"
+)
+
+type ClientDatum struct {
+	cbor.StructAsArray
+	ClientName []byte
+	Expiration uint
+	Credential []byte
+	Region     []byte
+}
+
+func (d *ClientDatum) UnmarshalCBOR(data []byte) error {
+	var tmpConstr cbor.Constructor
+	if _, err := cbor.Decode(data, &tmpConstr); err != nil {
+		return err
+	}
+	if tmpConstr.Constructor() != 0 {
+		return errors.New("invalid constructor")
+	}
+	type tClientDatum ClientDatum
+	var tmp tClientDatum
+	if _, err := cbor.Decode(tmpConstr.FieldsCbor(), &tmp); err != nil {
+		return err
+	}
+	*d = ClientDatum(tmp)
+	return nil
+}
